0cd89e822d39d51d06291a99473bcb60fdabd874
“gzuppa” <gzuppa86@gmail.com>
2025-07-18 09:10:26 -0500

    fix: favicon

--- src/components/common/BreadcrumbsService.tsx
+++ src/components/common/BreadcrumbsService.tsx
@@ -11,7 +11,12 @@ const BreadcrumbsService = () => {
               <h2 className="black fw_700 visible-slowly-right text-center">¿Quienes somos?</h2>
             </div>
             <div className="col-xl-12 col-lg-12 mt-20">
-              <h5 className="fw_700 visible-slowly-right" data-wow-delay="0.3s">
+              <h5 className="fw_700 visible-slowly-right d-none d-md-block" data-wow-delay="0.3s">
+                Somos una arrendadora mexicana laborando en el mercado financiero y empresarial por más de 25 años, los fundadores de LIMEX nos dimos cuenta de que queremos hacer del arrendamiento algo más simple, accesible y poder contribuir un poco para mejorar el mundo en el que vivimos.
+              </h5>
+            </div>
+            <div className="col-xl-12 col-lg-12 mt-20">
+              <h5 className="fw_700 text-justify d-block d-md-none">
                 Somos una arrendadora mexicana laborando en el mercado financiero y empresarial por más de 25 años, los fundadores de LIMEX nos dimos cuenta de que queremos hacer del arrendamiento algo más simple, accesible y poder contribuir un poco para mejorar el mundo en el que vivimos.
               </h5>
             </div>
